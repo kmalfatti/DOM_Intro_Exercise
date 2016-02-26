dff1f8f094ab3c1ef1a9b2f0ae216c2667b31572
Kyle Malfatti <kyle.malfatti@gmail.com>
2016-02-25 20:19:34 -0800

    added first 2 solutions

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("Javascript is alive!");
+
+
+function load() {
+  document.getElementById('greeting').innerHTML = 'Hello World!';
+  document.getElementById('essentials').style.backgroundColor = 'yellow';
+  var gif = document.createElement('img');
+  document.getElementById('greeting').appendChild(gif);
+  document.getElementsByTagName('img')[0].src = 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif';
+}
+window.onload = load();
+
+var li = document.getElementsByTagName('ul');
+function afterwords() {
+  for( var i=0; i<li.length; i++) {
+  li[i].className = 'selected';
+  li[i].removeEventListener('click', afterwords);
+  }
+}
+document.addEventListener('click', afterwords);
+
+
+// When the gray div is moused over, 
+// it's removed from the DOM.
+
